2fbd2755eade899c00a656b383f0f96bfbbbf8f6
John Russell <john.russell@mbp-jrussell.local>
2015-05-28 17:02:58 -0400

    more stuff works

--- untitled/app/js/directives/displayNav.js
+++ untitled/app/js/directives/displayNav.js
@@ -1,4 +1,4 @@
-app.directive('displayNav', ['$location', function($location){
+app.directive('displayNav', ['$location', '$route', function($location, $route){
   return {
     restrict: 'E',
     scope: {},
@@ -6,6 +6,7 @@ app.directive('displayNav', ['$location', function($location){
     link: function(scope, element, attrs) { 
       scope.onClick = function(clickedId) { 
         $location.path("/content/" + clickedId );
+        $route.reload();
       } 
     }
   };
