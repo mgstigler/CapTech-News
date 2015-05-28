6c7f620bfdf0e5bf6d60f28ec3a49cc70e4f8705
Maddie Stigler <maddie.stigler@mbp-mstigler.local>
2015-05-28 11:34:55 -0400

    Updated with Tabs

--- untitled/app/js/controllers/homeController.js
+++ untitled/app/js/controllers/homeController.js
@@ -8,8 +8,16 @@ app.controller('HomeController', ['$scope', 'content', function($scope, content)
     });
 }]);
 
-app.controller('CategoryController', ['$scope', 'category', '$routeParams', function($scope, category, $routeParams) {
-    content.success(function(data) {
-        $scope.category = data[$routeParams.id];
+app.controller('CategoryController', ['$scope', 'CategoriesService', '$routeParams', function($scope, CategoriesService, $routeParams) {
+
+    $scope.displayName = "hello";
+
+    CategoriesService.success(function(data) {
+        $scope.categories = data;
     });
+
+    //$scope.category = data[$routeParams.id];
+    //content.success(function(data) {
+    //
+    //});
 }]);
--- untitled/app/js/services/categories.js
+++ untitled/app/js/services/categories.js
@@ -2,7 +2,7 @@
  * Created by maddie.stigler on 5/27/15.
  */
 
-    app.factory('category', ['$http', function($http) {
+    app.factory('CategoriesService', ['$http', function($http) {
         return $http.get('http://html5news.herokuapp.com/articles/categories')
             .success(function(data) {
                 return data;
